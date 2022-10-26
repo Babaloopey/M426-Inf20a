87854e90d031e1cf571946205b9320a959d8655b
Rood <48691039+Babaloopey@users.noreply.github.com>
2022-10-26 13:33:33 +0200

    added tests for mean

--- Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
+++ Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
@@ -9,13 +9,40 @@ namespace Statistics.Tests
         [Fact]
         public void TestMeanOfTwoNumbers()
         {
-            List<int> numbers = new List<int> { 1, 3 };
-            double expected = 2;
+            List<int> numbers = new List<int> { 2, 3 };
+            double expected = 2.5;
             Average average = new Average();
 
             double actual = average.Mean(numbers);
 
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TeastMeanOfFiveNumbers()
+        {
+            // arrange 
+            List<int> numbers = new List<int> { 2, 3, 4, 5, 6 };
+            double expected = 4;
+            Average average = new Average();
+
+            // act
+            double actual = average.Mean(numbers);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMeanOfNoNumbers()
+        {
+            // arrange 
+            List<int> numbers = new List<int> {};
+            Average average = new Average();
+
+            // act && assert
+            Assert.Throws<ArgumentException>(() => average.Mean(numbers));
+
         }
     }
 }
