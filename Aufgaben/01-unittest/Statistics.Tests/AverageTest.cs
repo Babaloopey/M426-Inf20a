a8cc13825bda47744cfe06a8764fed2cb64090dc
Rood <48691039+Babaloopey@users.noreply.github.com>
2022-11-02 12:56:29 +0100

    implemented median and test

--- Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
+++ Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
@@ -44,5 +44,48 @@ namespace Statistics.Tests
             Assert.Throws<ArgumentException>(() => average.Mean(numbers));
 
         }
+
+        [Fact]
+        public void TestMedianForOddNumberOfElements()
+        {
+            // arrange 
+            List<int> numbers = new List<int> { 2, 3, 4, 5, 6 };
+            double expected = 4;
+            Average average = new Average();
+
+            // act
+            double actual = average.Median(numbers);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void TestMedianForEvenNumberOfElements()
+        {
+            // arrange 
+            List<int> numbers = new List<int> { 4,5,7,6,3,2 };
+            double expected = 4.5;
+            Average average = new Average();
+
+            // act
+            double actual = average.Median(numbers);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void TestMedianForNoElements()
+        {
+            // arrange 
+            List<int> numbers = new List<int> { };
+            Average average = new Average();
+
+            // act && assert
+            Assert.Throws<ArgumentException>(() => average.Median(numbers));
+        }
     }
 }
--- Aufgaben/01-unittest/Statistics/Average.cs
+++ Aufgaben/01-unittest/Statistics/Average.cs
@@ -23,5 +23,40 @@ namespace Statistics
             mean = (double)sum / numbers.Count;
             return mean;
         }
+
+        public double Median(List<int> numbers)
+        {
+            if(numbers.Count == 0)
+            {
+                throw new ArgumentException("Eine leere Liste wurde gesendet");
+            }
+            numbers.Sort();
+            if (isGerade(numbers.Count))
+            {
+                double numberOne = numbers[getMittlereZahl(numbers.Count) + 1];
+                double numberTwo = numbers[getMittlereZahl(numbers.Count)];
+                return getAverage(numberOne, numberTwo);
+            }
+            else
+            {
+                return numbers[getMittlereZahl(numbers.Count)];
+            }
+            return 0;
+        }
+
+        public bool isGerade(int number)
+        {
+            return (number % 2) == 0;
+        }
+
+        public int getMittlereZahl(int number)
+        {
+            return (int)Math.Ceiling((double)number / (double)2) - 1;
+        }
+
+        public double getAverage(double a, double b)
+        {
+            return (a + b) / 2;
+        }
     }
 }
