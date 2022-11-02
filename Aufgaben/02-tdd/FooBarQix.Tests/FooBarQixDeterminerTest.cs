7b79af027632c16e67b46239dbeb817f7a4c3d2e
Rood <48691039+Babaloopey@users.noreply.github.com>
2022-11-02 18:15:17 +0100

    Added Function Determine and Test

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -4,9 +4,35 @@ namespace FooBarQix.Tests
 {
     public class FooBarQixDeterminerTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        [InlineData(3, "FooFoo")]
+        [InlineData(4, "4")]
+        [InlineData(5, "BarBar")]
+        [InlineData(6, "Foo")]
+        [InlineData(7, "QixQix")]
+        [InlineData(8, "8")]
+        [InlineData(9, "Foo")]
+        [InlineData(10, "Bar")]
+        [InlineData(13, "Foo")]
+        [InlineData(15, "FooBarBar")]
+        [InlineData(21, "FooQix")]
+        [InlineData(33, "FooFooFoo")]
+        [InlineData(51, "FooBar")]
+        [InlineData(53, "BarFoo")]
+
+        public void Test1(int parameter, string expected)
         {
+            // arrange
+            var determiner = new FooBarQixDeterminer();
+
+            // act
+            string actual = determiner.Determine(parameter); 
+
+            // assert
+            Assert.Equal(expected, actual);
+
         }
     }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -2,6 +2,48 @@
 {
     public class FooBarQixDeterminer
     {
+        public string Determine(int input)
+        {
+            string answer = "";
+
+            answer += determineNenner(input, 3, "Foo");
+            answer += determineNenner(input, 5, "Bar");
+            answer += determineNenner(input, 7, "Qix");
+
+
+            string inputInString = input.ToString();
+            for(int i = 0; i < inputInString.Length; i++)
+            {
+                addWordIfNumberIsFound(inputInString[i], '3', "Foo");
+                addWordIfNumberIsFound(inputInString[i], '5', "Bar");
+                addWordIfNumberIsFound(inputInString[i], '7', "Qix");
+            }
+
+            return answer == "" ? input.ToString() : answer;
+        }
+
+        private bool isDividableThrough(double input, double dividor)
+        {
+            return input % dividor == 0;
+        }
+
+        private string determineNenner(double input, double dividor, string word)
+        {
+            if (isDividableThrough(input, dividor))
+            {
+                return word;
+            }
+            return "";
+        }
+
+        private string addWordIfNumberIsFound(char number, char targetNumber ,string word)
+        {
+            if (number == targetNumber)
+            {
+                return word;
+            }
+            return "";
+        }
 
     }
 }
